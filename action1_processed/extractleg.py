3139e90dca8fc165842e5031c527681c622e0f07
crizyli <liyufei6666@gmail.com>
2020-09-22 22:19:25 +0800

    0

---
+++ action1_processed/extractleg.py
@@ -0,0 +1,22 @@
+import json
+import os
+import sys
+
+path = 'C:/Users/83446/Desktop/sensor2/action1_processed/2d_keypoints_a1'
+
+for file in os.listdir(path):
+	current = os.path.join(path, file)
+	if os.path.isfile(current):
+		with open(current) as json_file:
+			data = json.load(json_file)
+			keypoints=data['people'][0]['pose_keypoints_2d']
+			with open('action1_keypoints.csv','ab') as f:
+				to_write = ''
+				for i in range(0, 75, 3):	
+					to_write += (str(keypoints[i]))
+					to_write += (',')
+					to_write += (str(keypoints[i+1]))
+					to_write += (',')
+				to_write = to_write[:-1]
+				to_write += ('\n')
+				f.write(to_write.encode())
