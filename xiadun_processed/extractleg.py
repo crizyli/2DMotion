60f7f1da0aaf262659d6200bc5c36afa49b95176
crizyli <liyufei6666@gmail.com>
2020-09-24 21:52:57 +0800

    out imgs

---
+++ xiadun_processed/extractleg.py
@@ -0,0 +1,22 @@
+import json
+import os
+import sys
+
+path = 'C:/Users/83446/Documents/2DMotion/xiadun_processed/2d_keypoints_xiadun'
+
+for file in os.listdir(path):
+	current = os.path.join(path, file)
+	if os.path.isfile(current):
+		with open(current) as json_file:
+			data = json.load(json_file)
+			keypoints=data['people'][0]['pose_keypoints_2d']
+			with open('xiadun_keypoints.csv','ab') as f:
+				to_write = ''
+				for i in range(0, 75, 3):	
+					to_write += (str(keypoints[i]))
+					to_write += (',')
+					to_write += (str(keypoints[i+1]))
+					to_write += (',')
+				to_write = to_write[:-1]
+				to_write += ('\n')
+				f.write(to_write.encode())
